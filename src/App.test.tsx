9558a7d07a4a061c8c1e6dd7450fa3274a7995d0
Christoph Görn <goern@redhat.com>
2022-11-21 14:41:11 +0100

    :bug: fix the App test
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Home page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const artifactsLinkElement = screen.getByText(/artifacts/i);
+
+  expect(artifactsLinkElement).toBeInTheDocument();
 });
